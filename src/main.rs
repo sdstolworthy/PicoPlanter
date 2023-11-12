474495dec8c688aab0a416eecbd7e399f929b88c
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-11-11 21:42:09 -0800

    kind of working in a way

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,10 @@ use bsp::{
     entry,
     hal::{
         adc::AdcPin,
-        //    clocks::{init_clocks_and_plls, Clock},
-        //    gpio::{bank0::Gpio28, AnyPin, SpecificPin},
+        clocks::{init_clocks_and_plls, Clock},
         pac,
         sio::Sio,
-        Adc,
+        Adc, Watchdog,
     },
 };
 
@@ -27,24 +26,25 @@ use bsp::{
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
-    //    let external_xtal_freq_hz = 12_000_000u32;
-    //    let clocks = init_clocks_and_plls(
-    //        external_xtal_freq_hz,
-    //        pac.XOSC,
-    //        pac.CLOCKS,
-    //        pac.PLL_SYS,
-    //        pac.PLL_USB,
-    //        &mut pac.RESETS,
-    //        &mut watchdog,
-    //    )
-    //    .ok()
-    //    .unwrap();
-    //
-    //    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -52,17 +52,10 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    //    let led_pin = pins.led.into_push_pull_output();
-    // Enable adc
+    info!("pins");
     let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
     // Configure one of the pins as an ADC input
     let mut adc_pin_0 = AdcPin::new(pins.gpio28.into_floating_input());
-    info!("pins");
 
     let mut reader: moisture_detector::GPIOMoistureReader<
         '_,
@@ -76,27 +69,26 @@ fn main() -> ! {
         >,
     > = GPIOMoistureReader::new(&mut adc, &mut adc_pin_0);
 
-    info!("{}", reader.get_moisture());
+    let mut relay_pin = pins.gpio16.into_push_pull_output();
 
-    //    info!("setting relay pin");
-    //    let mut relay_pin = pins.gpio15.into_push_pull_output();
-    //
-    //    info!("setting pin");
-    //    if relay_pin.is_low().unwrap() {
-    //        relay_pin.set_high().unwrap();
-    //        info!("set high");
-    //    } else {
-    //        relay_pin.set_low().unwrap();
-    //        info!("set set low");
-    //    }
+    info!("setting relay pin");
 
     // Read the ADC counts from the ADC channel
     loop {
-        info!("get moisture");
+        delay.delay_ms(500);
+        info!("setting pin");
+        if relay_pin.is_low().unwrap() {
+            relay_pin.set_high().unwrap();
+            info!("set high");
+        } else {
+            relay_pin.set_low().unwrap();
+            info!("set set low");
+        }
+
         //let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
         let moisture = reader.get_moisture();
         //let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
-        info!("{}", moisture);
+        //info!("{}", moisture);
     }
 }
 
--- src/moisture_detector.rs
+++ src/moisture_detector.rs
@@ -4,10 +4,6 @@ use rp_pico::hal::Adc;
 
 use embedded_hal::adc::{Channel, OneShot};
 
-pub trait Waterer {
-    fn water(&mut self) -> Option<()>;
-}
-
 pub struct GPIOMoistureReader<'a, W, S>
 where
     S: Channel<Adc, ID = u8>,
@@ -41,38 +37,5 @@ where
 {
     fn get_moisture(&mut self) -> W {
         self.adc.read(self.pin).unwrap()
-    }
-}
-
-pub struct MoistureLevelWaterer<'a, W>
-where
-    W: From<u16>,
-{
-    watering_threshold: u16,
-    moisture_delay: u8,
-    moisture_reader: &'a mut dyn MoistureReader<W>,
-}
-
-impl<'a, W> Waterer for MoistureLevelWaterer<'a, W>
-where
-    W: From<u16>,
-{
-    fn water(&mut self) -> Option<()> {
-        loop {
-            self.moisture_reader.get_moisture();
-        }
-    }
-}
-
-impl<'a, W> MoistureLevelWaterer<'a, W>
-where
-    W: From<u16>,
-{
-    pub fn new(moisture_reader: &'a mut dyn MoistureReader<W>) -> Self {
-        MoistureLevelWaterer {
-            watering_threshold: 2000,
-            moisture_delay: 200,
-            moisture_reader,
-        }
     }
 }
