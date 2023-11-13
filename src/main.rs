3ba1cacbf484a501efb28042bda0f9656b6422b6
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-11-12 16:52:58 -0800

    use embassy

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,146 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
-mod moisture_detector;
-
-use moisture_detector::{GPIOMoistureReader, MoistureReader};
-
-use defmt::*;
+use defmt::info;
 use defmt_rtt as _;
-use panic_probe as _;
-
-use rp_pico as bsp;
-
+use embassy_executor::Spawner;
+use embassy_rp::{
+    adc::{self, Adc, Channel, InterruptHandler},
+    bind_interrupts,
+    gpio::{Level, Output},
+    i2c::{self, Blocking, Config, I2c},
+    peripherals::I2C0,
+};
+use embassy_time::{Duration, Timer};
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    mono_font::{ascii::FONT_6X10, MonoTextStyle, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
+    primitives::{PrimitiveStyle, Rectangle},
     text::{Baseline, Text},
 };
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use itoa;
+use panic_probe as _;
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
-use bsp::{
-    entry,
-    hal::{
-        adc::AdcPin,
-        clocks::{init_clocks_and_plls, Clock},
-        gpio::{FunctionI2C, Pin, PullUp},
-        pac,
-        sio::Sio,
-        Adc, Watchdog,
-    },
-};
+bind_interrupts!(struct Irqs {
+    ADC_IRQ_FIFO => InterruptHandler;
+});
 
-use fugit::RateExtU32;
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    defmt::info!("Initializing...");
 
-#[entry]
-fn main() -> ! {
-    info!("Program start");
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    delay.delay_ms(1000);
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    info!("pins");
-    let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
-    // Configure one of the pins as an ADC input
-    let mut adc_pin_0 = AdcPin::new(pins.gpio28.into_floating_input());
-
-    let mut reader: moisture_detector::GPIOMoistureReader<
-        '_,
-        u16,
-        AdcPin<
-            bsp::hal::gpio::Pin<
-                bsp::hal::gpio::bank0::Gpio28,
-                bsp::hal::gpio::FunctionSio<bsp::hal::gpio::SioInput>,
-                bsp::hal::gpio::PullNone,
-            >,
-        >,
-    > = GPIOMoistureReader::new(&mut adc, &mut adc_pin_0);
-
-    // let mut relay_pin = pins.gpio16.into_push_pull_output();
-
-    // Configure two pins as being I²C, not GPIO
-    let sda_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio14.reconfigure();
-    let scl_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio15.reconfigure();
-    // let not_an_scl_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio20.reconfigure();
-
-    // Create the I²C drive, using the two pre-configured pins. This will fail
-    // at compile time if the pins are in the wrong mode, or if this I²C
-    // peripheral isn't available on these pins!
-    let i2c = rp_pico::hal::I2C::i2c1(
-        pac.I2C1,
-        sda_pin,
-        scl_pin, // Try `not_an_scl_pin` here
-        400.kHz(),
-        &mut pac.RESETS,
-        &clocks.system_clock,
-    );
-
+    defmt::info!("Initialized.");
+    let sda = p.PIN_16;
+    let scl = p.PIN_17;
+    let i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, Config::default());
     let interface = I2CDisplayInterface::new(i2c);
+
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
 
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
+    let mut oled = OledScreen::new(&mut display);
 
-    let initial_text = "Hello, world! ";
+    let mut adc = Adc::new(p.ADC, Irqs, adc::Config::default());
 
-    Text::with_baseline(initial_text, Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+    let mut p26 = Channel::new_pin(p.PIN_26, embassy_rp::gpio::Pull::None);
 
-    display.flush().unwrap();
-    info!("setting relay pin");
+    let mut buffer = itoa::Buffer::new();
+
+    let mut relay = Output::new(p.PIN_21, Level::High);
+
+    oled.write("Moisture Meter", 0);
+
+    oled.write("Current moisture:", 1);
 
     loop {
-        delay.delay_ms(500);
-        info!("setting pin");
-        // if relay_pin.is_low().unwrap() {
-        //     relay_pin.set_high().unwrap();
-        //     info!("set high");
-        //     Text::with_baseline("Hello world! Hi", Point::zero(), text_style, Baseline::Top)
-        //         .draw(&mut display)
-        //         .unwrap();
-        // } else {
-        //     relay_pin.set_low().unwrap();
-        //     info!("set set low");
-        //     Text::with_baseline("Hello world! Low", Point::zero(), text_style, Baseline::Top)
-        //         .draw(&mut display)
-        //         .unwrap();
-        // }
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
 
-        //let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
-        let moisture = reader.get_moisture();
-        info!("{}", moisture);
-        display.clear_buffer();
-        display.flush().unwrap();
-        Text::with_baseline("Hello, world", Point::zero(), text_style, Baseline::Top)
-            .draw(&mut display)
-            .unwrap();
-        display.flush().unwrap();
-        delay.delay_ms(500);
-        display.clear_buffer();
-        display.flush().unwrap();
+        let moisture_level = adc.read(&mut p26).await.unwrap();
 
-        Text::with_baseline("Hola", Point::zero(), text_style, Baseline::Top)
-            .draw(&mut display)
+        let moisture_desc = buffer.format(moisture_level);
+
+        info!("{}", moisture_level);
+
+        oled.write(moisture_desc, 2);
+
+        led.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+
+        if moisture_level > 2400 {
+            oled.write("Pump on", 3);
+            info!("turning on relay");
+            relay.set_low();
+        } else if moisture_level < 2000 {
+            oled.write("Pump off", 3);
+            info!("turning off relay");
+            relay.set_high();
+        }
+    }
+}
+
+struct OledScreen<'a> {
+    line_height: i32,
+    display: &'a mut Ssd1306<
+        I2CInterface<I2c<'a, I2C0, Blocking>>,
+        ssd1306::prelude::DisplaySize128x64,
+        BufferedGraphicsMode<ssd1306::prelude::DisplaySize128x64>,
+    >,
+    text_style: MonoTextStyle<'a, BinaryColor>,
+    clear_style: PrimitiveStyle<BinaryColor>,
+}
+
+impl<'a> OledScreen<'a> {
+    fn new(
+        display: &'a mut Ssd1306<
+            I2CInterface<I2c<'a, I2C0, Blocking>>,
+            ssd1306::prelude::DisplaySize128x64,
+            BufferedGraphicsMode<ssd1306::prelude::DisplaySize128x64>,
+        >,
+    ) -> Self {
+        let clear_style = PrimitiveStyle::with_fill(BinaryColor::Off);
+        let text_style = MonoTextStyleBuilder::new()
+            .font(&FONT_6X10)
+            .text_color(BinaryColor::On)
+            .build();
+        OledScreen {
+            line_height: 16,
+            display,
+            text_style,
+            clear_style,
+        }
+    }
+
+    fn get_line_bound(&mut self, line: i32) -> Point {
+        Point::new(0, line * self.line_height)
+    }
+    pub fn write(&mut self, s: &str, line: i32) -> () {
+        let line_start = self.get_line_bound(line);
+        self.clear_line(line);
+        self.display.flush().unwrap();
+        Text::with_baseline(s, line_start, self.text_style, Baseline::Top)
+            .draw(self.display)
             .unwrap();
-        display.flush().unwrap();
+        self.update();
+    }
+    fn clear_line(&mut self, line: i32) -> () {
+        let width = self.display.size().width;
+        Rectangle::new(
+            self.get_line_bound(line),
+            Size::new(width, self.line_height as u32),
+        )
+        .into_styled(self.clear_style)
+        .draw(self.display)
+        .unwrap();
+    }
+    fn update(&mut self) -> () {
+        self.display.flush().unwrap();
     }
 }
